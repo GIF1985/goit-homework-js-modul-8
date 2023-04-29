9ee69d371da703e6e237dae44994a2c0b86ec2a0
GIF1985 <125868361+GIF1985@users.noreply.github.com>
2023-04-29 16:50:53 +0300

    Задание 2 - видео плеер

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+// Импортируем библиотеку Vimeo плеера
+import Player from '@vimeo/player';
+// Импортируем функцию throttle из библиотеки lodash.throttle
+import throttle from 'lodash.throttle';
 
+// Создаем экземпляр класса Player и сохраняем его в переменную player
+const player = new Player('vimeo-player');
+
+// Добавляем обработчик события timeupdate,
+// который вызывается при каждом обновлении времени воспроизведения
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    // Сохраняем текущее время воспроизведения в локальное хранилище
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  }, 1000)
+);
+
+// При перезагрузке страницы проверяем, есть ли сохраненное время воспроизведения
+const currentTime = localStorage.getItem('videoplayer-current-time');
+// Если есть, устанавливаем его в качестве текущего времени воспроизведения
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
